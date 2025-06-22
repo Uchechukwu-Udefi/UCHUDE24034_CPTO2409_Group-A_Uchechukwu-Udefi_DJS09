cda268072cd0f04ed4333b5a05318a95262ea05d
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-22 12:40:23 +0200

    Interface and class challenge

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@
 // : number
 import { mostRecentViewer, populateUser, showDetails, getTopTwoReviews } from './utils'
 import { Permissions, LoyaltyUser } from './enums'; 
-import { Price, Country } from './types'
+import { Reviews, Property } from './interfaces';
+
 const propertyContainer = document.querySelector('.properties')
 const reviewContainer = document.querySelector('.reviews')
 const container = document.querySelector('.container')
@@ -19,7 +20,7 @@ let isLoggedIn : boolean
   stars: number;
   loyaltyUser: LoyaltyUser;
   date: string;
-}*/const reviews: any [] = [
+}*/const reviews: Reviews[] = [
   {
     name: "Sheia",
     stars: 5,
@@ -50,19 +51,7 @@ const you = {
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow'],
 }
 
-const properties : {
-    image: string;
-    title: string;
-    price: number;
-    location: {
-        firstLine: string;
-        city: string;
-        code: number;
-        country: string;
-    };
-    contact: [number, string];
-    isAvailable: boolean;
-}[] = [
+const properties : Property[] = [
   {
     image: 'images/colombia-property.jpg',
         title: 'Colombian Shack',
@@ -102,6 +91,19 @@ const properties : {
         contact: [ +1123495082908, 'andyluger@aol.com'],
         isAvailable: true
   },
+  {
+        image: 'images/malaysian-hotel.jpeg',
+        title: 'Malia Hotel',
+        price: 35,
+        location: {
+            firstLine: 'Room 4',
+            city: 'Malia',
+            code: 45334,
+            country: 'Malaysia'
+        },
+        contact: [ +60349822083, 'lee34@gmail.com'],
+        isAvailable: false
+    },
 ]
 
 //Function
@@ -122,17 +124,14 @@ for (let i = 0; i < properties.length; i++) {
         const image = document.createElement('img')
         image.setAttribute('src', properties[i].image)
         card.appendChild(image)
-        propertyContainer.appendChild(card)
         showDetails(you.permissions, card, properties[i].price)
+        propertyContainer.appendChild(card)
+        
     };
+    
 
 let count = 0
-function addReviews(array: {
-    name: string;
-    stars: number;
-    loyaltyUser: LoyaltyUser;
-    date: string;
-}[] ) : void {
+function addReviews(array: Reviews[] ) : void {
     if (!count ) {
         count++
         const topTwo = getTopTwoReviews(array)
@@ -150,3 +149,31 @@ button.addEventListener('click', () => addReviews(reviews))
 
 let currentLocation: [string, string, number] = ['Cape Town', '11:35', 17]
 footer.innerHTML = currentLocation[0] + '|' + currentLocation[1] + '|' + currentLocation[2] + '°'
+
+
+// Classes
+class MainProperty {
+    src: string
+    title: string
+    reviews: Reviews[]
+    constructor(src: string, title: string, reviews: Reviews[]) {
+        this.src = src
+        this.title = title
+        this.reviews = reviews
+    }
+}
+
+let yourMainProperty = new MainProperty(
+    'images/italian-property.jpg', 
+    'Italian House',
+    [{
+        name: 'Olive',
+        stars: 5,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
+        date: '12-04-2021'
+    }] )
+
+const mainImageContainer = document.querySelector('.main-image')
+const image = document.createElement('img')
+image.setAttribute('src', yourMainProperty.src)
+mainImageContainer.appendChild(image)
